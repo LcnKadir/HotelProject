a704f584a80a1d534ae2027dad55e3cb7e80e7c0
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-07-26 15:06:27 +0300

    TestimonialController => Crud

--- ApiConsume/HotelProject.WebApi/Controllers/TestimonialController.cs
+++ ApiConsume/HotelProject.WebApi/Controllers/TestimonialController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelProject.WebApi.Controllers
@@ -7,5 +9,47 @@ namespace HotelProject.WebApi.Controllers
     [ApiController]
     public class TestimonialController : ControllerBase
     {
+        private readonly ITestimonialService _testimonialService;
+
+        public TestimonialController(ITestimonialService testimonialService)
+        {
+            _testimonialService = testimonialService;
+        }
+
+        [HttpGet]
+        public IActionResult ListTestimonial()
+        {
+            return Ok(_testimonialService.TGetList());
+        }
+
+        [HttpPost]
+        public IActionResult AddTestimonial(Testimonial Testimonial)
+        {
+            _testimonialService.TInsert(Testimonial);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var values = _testimonialService.TGetByID(id);
+            _testimonialService.TDelete(values);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTestimonial(Testimonial Testimonial)
+        {
+            _testimonialService.TUpdate(Testimonial);
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTestimonial(int id)
+        {
+            var values = _testimonialService.TGetByID(id);
+            return Ok(values);
+        }
     }
 }
+
