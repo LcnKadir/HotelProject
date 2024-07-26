18c5ff66151c9be128662115a2f273b6e9eb6d0b
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-07-26 14:40:57 +0300

    StaffController (Add and List) and configuration additions to the DI Container.

--- ApiConsume/HotelProject.WebApi/Program.cs
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -1,4 +1,8 @@
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.BusinessLayer.Concrete;
+using HotelProject.DataAccessLayer.Abstract;
 using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
@@ -10,6 +14,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//Configurations//
+builder.Services.AddDbContext<Context>();
+builder.Services.AddScoped<IStaffDal, EfStaffDal>();
+builder.Services.AddScoped<IStaffService, StaffManager>();
 
 
 //Database connection path//
