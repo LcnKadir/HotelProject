854abaacde5ebd63fc46ff5d3b34d198d1352a28
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-07-29 19:47:40 +0300

    ServiceController/Index => AddService

--- Frontend/HotelProject.WebUI/Dtos/ServiceDto/CreateServiceDto.cs
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/CreateServiceDto.cs
@@ -11,6 +11,7 @@ namespace HotelProject.WebApi.Dtos.ServiceDto
         [StringLength(100, ErrorMessage ="Hizmet başlığı en fazla 100 karakter olabilir")]
         public string Title { get; set; }
 
+        [Required(ErrorMessage = "Hizmet açıklaması giriniz")]
         public string Description { get; set; }
     }
 }
