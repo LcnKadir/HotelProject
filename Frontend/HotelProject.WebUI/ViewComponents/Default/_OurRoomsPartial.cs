f490f2d5f8f030bf5ecd0ca9adeb0251073254d0
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-09-14 19:48:47 +0300

    WebUI => _OurRoomsPartial / Default -> Rooms are listed on the home page

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_OurRoomsPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_OurRoomsPartial.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.AboutDto;
+using HotelProject.WebUI.Dtos.RoomDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _OurRoomsPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public readonly IHttpClientFactory _httpClientFactory;
+
+        public _OurRoomsPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsemesagge = await client.GetAsync("http://localhost:5219/api/Room");
+
+            if (responsemesagge.IsSuccessStatusCode)
+            {
+                var jsonData = await responsemesagge.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultRoomDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
