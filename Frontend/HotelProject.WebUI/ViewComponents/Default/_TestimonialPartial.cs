c7304ba2156805de48c8777b6414fb6b4c671687
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-09-16 15:49:26 +0300

    Web UI => _TestimonialPartial/ Default -> Testimonals listed statically on the home page

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_TestimonialPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_TestimonialPartial.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebApi.Dtos.ServiceDto;
+using HotelProject.WebUI.Dtos.TestimonialDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _TestimonialPartial: ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsemesagge = await client.GetAsync("http://localhost:5219/api/Testimonial");
+
+            if (responsemesagge.IsSuccessStatusCode)
+            {
+                var jsonData = await responsemesagge.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
