490d2e27f4f64a235fbfac58735ea4bdebd7bbe6
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-09-16 14:45:46 +0300

    Web UI => _ServicePartial / Default -> entered hotel services are statically listed on the home page

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_ServicePartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_ServicePartial.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebApi.Dtos.ServiceDto;
+using HotelProject.WebUI.Dtos.RoomDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _ServicePartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServicePartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsemesagge = await client.GetAsync("http://localhost:5219/api/Service");
+
+            if (responsemesagge.IsSuccessStatusCode)
+            {
+                var jsonData = await responsemesagge.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
