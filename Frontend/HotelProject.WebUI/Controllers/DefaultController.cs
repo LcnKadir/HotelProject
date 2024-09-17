524df85f643b1cc163dbd4dbd5dbc33fd001bcfd
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-09-17 15:45:48 +0300

    Web UI => DefaultController -> _SubscribePartial / _SubscribePartial.cshmtl was edited and the API was enabled to be consumed. Subscription to the newsletter via e-mail was made on the home page.

--- Frontend/HotelProject.WebUI/Controllers/DefaultController.cs
+++ Frontend/HotelProject.WebUI/Controllers/DefaultController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebApi.Dtos.ServiceDto;
+using HotelProject.WebUI.Dtos.SubscribeDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class DefaultController : Controller
     {
+        public readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -17,9 +28,18 @@ namespace HotelProject.WebUI.Controllers
         }
 
         [HttpPost]
-        public IActionResult _SubscribePartial()
+        public async Task<IActionResult> _SubscribePartial(CreateSubscribeDto dto)
         {
-            return PartialView();
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(dto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responsemessage = await client.PostAsync("http://localhost:5219/api/Subscribe", stringContent);
+
+            if (responsemessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
         }
     }
 }
