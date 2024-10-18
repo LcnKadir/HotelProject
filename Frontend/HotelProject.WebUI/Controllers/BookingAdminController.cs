003c41666e3d3d4d99535e245d492e0d5a027471
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-10-18 16:37:27 +0300

    BookingAdminController / Index -> ResultDto was created and the data was consumed in the controller and listed on the page.

--- Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
+++ Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebApi.Dtos.ServiceDto;
+using HotelProject.WebUI.Dtos.BookingDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class BookingAdminController : Controller
     {
-        public IActionResult Index()
+        public readonly IHttpClientFactory _httpClientFactory;
+
+        public BookingAdminController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsemesagge = await client.GetAsync("http://localhost:5219/api/Booking");
+
+            if (responsemesagge.IsSuccessStatusCode)
+            {
+                var jsonData = await responsemesagge.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
