87a75397e88b71257ebd2a654a466a3951983161
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-10-31 11:43:36 +0300

    WebApiJwt => programcs -> JWT Structure and Configuration

--- WebApiJwt/Program.cs
+++ WebApiJwt/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//JWT Yapýlanmasý ve Konfigürasyonu //JWT Structure and Configuration
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.RequireHttpsMetadata = false;
+    opt.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidIssuer = "http://localhost",
+        ValidAudience = "http://localhost",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("aspnetcoreapiapi")), //Gelen kullanýcý bu key'e sahipse iþlem gerçekleþtirilecek. 
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true
+    };
+});
 
 var app = builder.Build();
 
